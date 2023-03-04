f752bc19cc76fa5483ab1cce3487fd9b18d32004
qoudri4re <qoudri4re@gmail.com>
2023-03-04 16:05:56 +0100

    allowed hosts

--- clonestagram/settings.py
+++ clonestagram/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clonestagram-api.onrender.com']
 
 
 # Application definition
