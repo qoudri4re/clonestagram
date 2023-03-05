8b22cc427d9eedf90349eec413a2a61d52b1001e
qoudri4re <qoudri4re@gmail.com>
2023-03-05 14:35:50 +0100

    fix

--- clonestagram/settings.py
+++ clonestagram/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['clonestagram-api.onrender.com']
+ALLOWED_HOSTS = ['clonestagram-api.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
